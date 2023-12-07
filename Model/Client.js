1ccf5b1e41f61bb391e8f7f81b0f64bea10b9b81
vohizy <hei.vohizy@gmail.com>
2023-12-07 09:51:05 +0300

    feat: methode add transaction for a client

--- Model/Client.js
+++ Model/Client.js
@@ -5,5 +5,15 @@ class Client {
     this.portefeuille = portefeuille;
     this.transactions = [];
   }
+
+  ajouterTransaction(montant, description, siteInternet) {
+    const transaction = {
+      montant,
+      description,
+      siteInternet,
+      date: new Date(),
+    };
+    this.transactions.push(transaction);
+  }
 }
 module.exports = Client;
