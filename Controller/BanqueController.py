6803ac87b7868f62c5c9cbb49777f4464e97b5cd
vohizy <hei.vohizy@gmail.com>
2023-12-07 10:55:58 +0300

    feat: the methodes add client and trasactional in Bank

--- Controller/BanqueController.py
+++ Controller/BanqueController.py
@@ -1,4 +1,18 @@
+from Model.Client import Client
+from Model.PortefeuilleElectronique import PortefeuilleElectronique
+
 class BanqueController:
     def __init__(self):
         self.clients = []
         self.solde_total = 0
+
+    def ajouter_client(self, client):
+        self.clients.append(client)
+
+    def effectuer_transaction(self, client, montant, description, site_internet):
+        if client.portefeuille.retirer(montant):
+            client.ajouter_transaction(montant, description, site_internet)
+            self.solde_total += montant
+            return True
+        else:
+            return False
